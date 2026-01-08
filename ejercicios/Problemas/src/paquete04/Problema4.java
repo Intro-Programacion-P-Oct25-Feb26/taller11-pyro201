687e3a0c0811e687664d4edf9501dc66d1ed346c
jonathan <jonathanjavier2004@hotmail.com>
2026-01-08 12:46:44 -0500

    problema4

--- ejercicios/Problemas/src/paquete04/Problema4.java
+++ ejercicios/Problemas/src/paquete04/Problema4.java
@@ -5,17 +5,59 @@
  */
 package paquete04;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema4 {
 
+    static Scanner entrada = new Scanner(System.in);
+
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int operacion;
+        System.out.println("ingrese el valor que desea calcular,(1) planilla de luz"
+                + " (2) predio de un inmueble ");
+        operacion = entrada.nextInt();
+        entrada.nextLine();
+        if (operacion == 1) {
+            System.out.println("ingrese su nombre y apellido");
+            String name = entrada.nextLine();
+            System.out.println("ingrese su numero de cedula");
+            int cedu = entrada.nextInt();;
+            planilla(name, cedu);
+        } else if (operacion == 2) {
+            System.out.println("ingrese su nombre y apellido");
+            String name = entrada.nextLine();
+            System.out.println("ingrese su numero de cedula");
+            int cedu = entrada.nextInt();
+            predio(name, cedu);
+        } else {
+            System.out.println("ingrese un valor correspondiente al valor que"
+                    + " desea calcular");
+        }
     }
-    
+
+    public static void planilla(String a, int b) {
+        System.out.println("ingrese el valor del kilovatio");
+        double precio = entrada.nextDouble();
+        System.out.println("ingrese la catidad de kilovatios del mes");
+        double kilov = entrada.nextDouble();
+        double suma = precio * kilov;
+        System.out.printf("Cliente %s con cedula %s debera cancelar el valor de $%s\n",
+                a, b, suma);
+    }
+
+    public static void predio(String a, int b) {
+        System.out.println("ingrese el valor de su inmueble");
+        double precio = entrada.nextDouble();
+        double predio = precio * .02;
+        System.out.printf("Cliente %s con cedula %s tene un inmueble valorado en"
+                + " $%s y tiene que pagar un predio de $%s\n",
+                a, b, precio, predio);
+    }
 }
