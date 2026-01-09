bf47b1c766a8148da7ff727a894756087fdc1182
jonathan <jonathanjavier2004@hotmail.com>
2026-01-09 17:52:36 -0500

    problemas 5 y 6

--- ejercicios/Problemas/src/paquete05/Problema5.java
+++ ejercicios/Problemas/src/paquete05/Problema5.java
@@ -10,8 +10,30 @@ package paquete05;
  * @author reroes
  */
 public class Problema5 {
-    
-int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
-int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
-int [][] suma = sumas(informacion, informacion2);
+
+    public static void main(String[] args) {
+        int[][] informacion = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] informacion2 = {{1, 2, 3}, {10, 20, 30}, {100, 200, 300}};
+        int[][] sumaf = sumas(informacion, informacion2);
+        presentar(sumaf);
+    }
+
+    public static int[][] sumas(int[][] a, int[][] b) {
+        int[][] suma = new int[3][3];
+        for (int i = 0; i < a.length; i++) {
+            for (int c = 0; c < a[i].length; c++) {
+                int z = a[i][c] + b[i][c];
+                suma[i][c] = z;
+            }
+        }
+        return suma;
+    }
+    public static void presentar(int[][] a) {
+        for (int i = 0; i < a.length; i++) {
+            for (int c = 0; c < a[i].length; c++) {
+                System.out.printf("%s\t ",a[i][c]);
+            }
+            System.out.println("");
+        }
+    }
 }
--- ejercicios/Problemas/src/paquete06/Problema6.java
+++ ejercicios/Problemas/src/paquete06/Problema6.java
@@ -10,5 +10,38 @@ package paquete06;
  * @author reroes
  */
 public class Problema6 {
-    
+
+    public static void main(String[] args) {
+        int[] informacion = {1, 2, 3, 10, 20, 30, 100, 200, 300};
+
+        double promedio = prom(informacion);
+
+        double desviacion = desvia(informacion, promedio);
+
+        System.out.printf("Media aritmetica: %.2f\n", promedio);
+        System.out.printf("Desviacion estandar: %.2f\n", desviacion);
+    }
+
+    public static double prom(int[] a) {
+        int suma = 0;
+        double promedio;
+        for (int i = 0; i < a.length; i++) {
+            suma = suma + a[i];
+
+        }
+        promedio = suma / a.length;
+        return promedio;
+
+    }
+
+    public static double desvia(int[] a, double b) {
+        double suma = 0;
+        for (int i = 0; i < a.length; i++) {
+            suma = suma + Math.pow(a[i] - b, 2);
+        }
+        double v = suma / a.length;
+        return Math.sqrt(v);
+
+    }
+
 }
