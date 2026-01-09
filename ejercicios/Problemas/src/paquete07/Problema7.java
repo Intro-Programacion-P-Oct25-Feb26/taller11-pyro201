4068c92b740f0a8650fca8d4db7e9ecfc704e58c
jonathan <jonathanjavier2004@hotmail.com>
2026-01-09 18:09:43 -0500

    problemas 7 y final

--- ejercicios/Problemas/src/paquete07/Problema7.java
+++ ejercicios/Problemas/src/paquete07/Problema7.java
@@ -5,10 +5,44 @@
  */
 package paquete07;
 
+import java.util.Scanner;
+
 /**
  *
  * @author reroes
  */
 public class Problema7 {
-    
+
+    static Scanner entrada = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        String[] ciudades = matriz_ciu();
+        imprimir(ciudades);
+
+    }
+
+    public static String[] matriz_ciu() {
+        int val;
+        System.out.println("Ingrese el número de elementos del arreglo");
+        val = entrada.nextInt();
+
+        entrada.nextLine();
+        String[] city = new String[val];
+        for (int i = 0; i < city.length; i++) {
+            System.out.println("Ingrese el nombre de las ciudades");
+            city[i] = entrada.nextLine();
+        }
+        return city;
+    }
+
+    public static void imprimir(String[] a) {
+        System.out.println("Ciudades con 4 o 5 caracteres:");
+        for (int i = 0; i < a.length; i++) {
+            int condi = a[i].length();
+            if ((condi == 4) || (condi == 5)) {
+                System.out.println(a[i]);
+            }
+        }
+
+    }
 }
